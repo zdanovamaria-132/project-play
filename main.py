50c0460f12970fb5714807ff32d19500c7021f3b
Your Name <you@example.com>
2025-01-11 15:38:15 +0300

    Исправлено выделение объектов, неправильный диапазон sRGB

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import pygame
 import sys
 
-# Инициализация Pygame
 pygame.init()
 
-# Установите размеры окна
-screen = pygame.display.set_mode((1024, 1024))
+screen = pygame.display.set_mode((750, 750))
 
-# Установите заголовок окна
 pygame.display.set_caption("Главное окно игры")
 
-# Загрузите изображение фона из папки data
 background = pygame.image.load('data/background.png')
+background1 = pygame.image.load('data/background1.png')
+background2 = pygame.image.load('data/background2.png')
 
-# Основной цикл игры
+
+cursor = pygame.image.load('data/cursor.png')
+cursor_rect = cursor.get_rect()
+
+pygame.mouse.set_visible(False)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Отобразите изображение фона
-    screen.blit(background, (0, 0))
+    cursor_rect.topleft = pygame.mouse.get_pos()
 
-    # Обновите экран
+    if (249 < cursor_rect.x < 319 and 510 < cursor_rect.y < 560 or
+            178 < cursor_rect.x < 313 and 560 < cursor_rect.y < 615 or
+            165 < cursor_rect.x < 460 and 615 < cursor_rect.y < 750):
+        screen.blit(background1, (0, 0))
+    elif (560 < cursor_rect.x < 605 and 305 < cursor_rect.y < 395 or
+            552 < cursor_rect.x < 630 and 395 < cursor_rect.y < 485):
+        screen.blit(background2, (0, 0))
+    else:
+        screen.blit(background, (0, 0))
+
+    screen.blit(cursor, cursor_rect)
+
     pygame.display.flip()
